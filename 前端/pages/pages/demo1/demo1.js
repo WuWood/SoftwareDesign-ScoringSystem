f16d58087c4dc8338d13f5f324f7f1138894e0e4
WD <79662527+gdmu666@users.noreply.github.com>
2021-04-22 00:44:01 +0800

    首页基本定型
    存在一个BUG，以及需要把样式移到wxss里面

---
+++ 前端/pages/pages/demo1/demo1.js
@@ -0,0 +1,78 @@
+// pages/demo1/demo1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    time:'',
+    time1:''
+  },
+  bindMultiPickerChange1:function(e){
+    console.log(e.detail.value)
+    this.setData({
+      time1:e.detail.value
+    })
+  },
+  bindMultiPickerChange:function(e){
+    console.log(e.detail.value)
+    this.setData({
+      time:e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
